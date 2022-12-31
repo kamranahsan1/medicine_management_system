dcd94afb52c31cb5096d97426c96a3e78215aab8
kamranahsan <kamranahsan75@gmail.com>
2022-12-31 18:20:48 +0400

    initial commit

---
+++ resources/views/layout/partials/breadcrumbs.blade.php
@@ -0,0 +1,3 @@
+<ol class="breadcrumb float-sm-right">
+    <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+</ol>
---
+++ resources/views/layout/partials/footer.blade.php
@@ -0,0 +1,1 @@
+ <footer class="main-footer"><strong>Copyright &copy; 2022 Medicine Management System.</strong>All rights reserved.</footer>
